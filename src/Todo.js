b2f3b55113c11f9c13b06cbc06bfee95e2805aa0
anchalnalwale <nalwaleanchal@gmail.com>
2025-06-21 21:40:09 +0530

    Initial commit

---
+++ src/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div>
+        <input placeholder='Add a task' type='text'/>
+        <button>Add Task</button>
+        <br/>
+        <br/>
+        <br/>
+        <h4>To Do List</h4>
+    </div>
+  )
+}
+
+export default Todo
