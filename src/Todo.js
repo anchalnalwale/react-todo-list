93bec2e8466837138851d2d9493a3738269849b3
anchalnalwale <nalwaleanchal@gmail.com>
2025-06-22 16:05:38 +0530

    Done changes

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 const Todo = () => {
+  let [todos , setTodos] = useState([{task : "sample-task" , id : uuidv4() , isDone: false}]);
+  let [newTodo , setNewTodo] = useState("");
+    let addNewTask = () => {
+        setTodos ((prevTodos) => {
+            return [...prevTodos, {task : newTodo , id : uuidv4() , isDone: false}];
+        })
+        setNewTodo("");
+    };
   return (
-    <div>
-        <input placeholder='Add a task' type='text'/>
-        <button>Add Task</button>
-        <br/>
-        <br/>
-        <br/>
-        <h4>To Do List</h4>
+    <div className='flex flex-col justify-center items-center mt-32'>
+      <input placeholder='Add a task' type='text' />
+      <button onClick={addNewTask}>Add Task</button>
+      <br />
+      <br />
+      <br />
+      <h4>To Do List</h4>
+      <ul>
+
+      </ul>
     </div>
   )
 }
